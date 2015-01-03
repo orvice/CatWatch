6ce7e0b66dda50b583ccbfa9f4a00fb450ac15f9
orvice <orvice@gmail.com>
2015-01-03 20:42:31 +0800

    add config.php

--- client/api.php
+++ client/api.php
@@ -1,6 +1,7 @@
 <?php
 header("Content-type: text/html; charset=utf-8");
 require_once 'lib/class/sys.class.php';
+require_once 'lib/config.php';
 $api = new sys();
 $o = $api->do_sys();
 $json = json_encode($o);
